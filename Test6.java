b110cb400a789ee9dcccd3be71bb4aa7dd8eed2a
arshita <arshitapatel077@gmail.com>
2025-02-12 10:30:05 +0530

    Add files via upload

---
+++ Test6.java
@@ -0,0 +1,12 @@
+public class Test6 {
+    public static void main(String[] args) {
+        int a=10;
+        int b=20;
+        if(a<b){
+            System.out.println(b);
+        }else{
+            System.out.println(a);
+        }
+    }
+    
+}
