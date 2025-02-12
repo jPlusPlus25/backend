f700b52d637de90e6363449cbc56599868e754aa
arshita <arshitapatel077@gmail.com>
2025-02-12 10:30:24 +0530

    Add files via upload

---
+++ Test7.java
@@ -0,0 +1,12 @@
+public class Test7 {
+    public static void main(String[] args) {
+        int a=10;
+        int b=5;
+        if(a<b){
+            System.out.println(a);
+        }else{
+            System.out.println(b);
+        }
+    }
+    
+}
