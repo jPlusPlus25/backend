e663c0a456b3e6548309f16dd8ca7dcbc26d68d3
arshita <arshitapatel077@gmail.com>
2025-02-12 10:26:30 +0530

    Add files via upload

---
+++ Test2.java
@@ -0,0 +1,11 @@
+public class Test2 {
+    public static void main(String[] args) {
+        int a=15;
+        if(a%3==0){
+            System.out.println("yes");
+        }else{
+            System.out.println("No");
+        }
+    }
+    
+}
