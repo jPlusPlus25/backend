9d94b71add726e85a730b1137806fc8e9dffc83a
arshita <arshitapatel077@gmail.com>
2025-02-12 10:25:41 +0530

    Add files via upload

---
+++ Test1.java
@@ -0,0 +1,10 @@
+public class Test1{
+    public static void main (String [] args){
+        int a=14;
+        if(a%7==0){
+            System.out.println("divisible by 7: "+ a);
+        }else{
+            System.out.println("Not divisible by 7: "+a);
+        }
+    }
+}
