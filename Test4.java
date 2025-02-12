8f25ba1db9f36d7f33fa94ff5c98ebecaf699c9d
arshita <arshitapatel077@gmail.com>
2025-02-12 10:27:21 +0530

    Add files via upload

---
+++ Test4.java
@@ -0,0 +1,11 @@
+public class Test4 {
+    public static void main(String[] args) {
+        int a=123;
+        if(a>=100 && a<=999 || a>=-100 && a<=-999){
+            System.out.println("Yes");
+        }else{
+            System.out.println("No");
+        }
+    }
+    
+}
