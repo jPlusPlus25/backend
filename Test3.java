7517f32a0de81fe0c1bc13702cf89c960a8cd217
arshita <arshitapatel077@gmail.com>
2025-02-12 10:26:54 +0530

    Add files via upload

---
+++ Test3.java
@@ -0,0 +1,11 @@
+public class Test3 {
+    public static void main(String[] args) {
+        int a=-1;
+        if(a>0){
+            System.out.println("Possitive");
+        }else{
+            System.out.println("Negative");
+        }
+    }
+    
+}
