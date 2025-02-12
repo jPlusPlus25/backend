2ad4a7ab2a1bbb19a3d3e82510fad70a2d54d450
arshita <arshitapatel077@gmail.com>
2025-02-12 10:29:40 +0530

    Add files via upload

---
+++ Test5.java
@@ -0,0 +1,11 @@
+public class Test5 {
+    public static void main(String[] args) {
+        int a=5;
+        if(a%2==0){
+            System.out.println("Even");
+        }else{
+            System.out.println("Odd");
+        }
+    }
+    
+}
