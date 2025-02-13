4571d2f4758695ffc68dffc749ff2ef12fc89db6
arshita <arshitapatel077@gmail.com>
2025-02-13 09:15:19 +0530

    Add files via upload

---
+++ Test12.java
@@ -0,0 +1,15 @@
+public class Test12 {
+    public static void main(String[] args) {
+        int a=4;
+        int b=9;
+        int c=18;
+        if(a>b && a>c){
+            System.out.println(a);
+        }else if(b>a && b>c){
+            System.out.println(b);
+        }else{
+            System.out.println(c);
+        }
+    }
+    
+}
